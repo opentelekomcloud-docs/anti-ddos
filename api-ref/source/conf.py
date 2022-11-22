9a6c1df0dc9879aac0f1fc5871ee531e882778f5
Artem Goncharov <artem.goncharov@gmail.com>
2022-11-22 13:43:14 +0100

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -28,8 +28,6 @@ project = 'Anti DDoS'
 otcdocs_repo_name = 'opentelekomcloud-docs/anti-ddos'
 # Those variables are required for edit/bug links
 
-
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -93,7 +91,7 @@ html_static_path = ['_static']
 
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
-('index',
+    ('index',
      'antiddos-api-ref.tex',
      u'Anti DDoS - API Reference',
      u'OpenTelekomCloud', 'manual'),
