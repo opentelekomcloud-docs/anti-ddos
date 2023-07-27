9f8e81ebffbe73668319d3914dc5523743264db3
Sebastian Gode <sebastian.gode@telekom.de>
2023-07-27 12:37:37 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,6 +29,17 @@ project = 'Anti DDoS'
 otcdocs_repo_name = 'opentelekomcloud-docs/anti-ddos'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = ''
+otcdocs_doc_link = ''
+otcdocs_doc_title = ''
+otcdocs_doc_type = ''
+otcdocs_service_category = 'security-services'
+otcdocs_service_title = 'Anti DDoS'
+otcdocs_service_type = 'antiddos'
+otcdocs_search_environment = 'hc_de'
+otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
